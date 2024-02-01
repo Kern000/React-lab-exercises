d8d33245df2daa41de82c8d5de98f20f3bbcd9d3
Kern_9b09 <holupkern@gmail.com>
2024-02-01 17:00:32 +0800

    completed labs 9b

---
+++ 09b-controlled-component/src/AddTaskForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function AddTaskForm(props){
+
+    return (
+        <React.Fragment>
+        <h2>Create new Task</h2>
+        <div>
+            <label>Task Description</label>
+            <input  type="text"
+                    name="newTaskName"
+                    value={props.newTask}
+                    onChange={(event) => props.setNewTask(event.target.value)}
+            />
+            <button onClick={props.addTask}>Add</button>
+            <p> "This is the new Task state:" {props.newTask} </p>
+        </div>
+        </React.Fragment>
+    )
+}
